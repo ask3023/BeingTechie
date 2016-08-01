a52046626cad5c33acb0841b847e0fd79f4ca5a5
ask3023 <sravan.ask@gmail.com>
2016-08-01 00:16:51 -0400

    ASP.NET Configuration

---
+++ ASPNETConfiguration/src/ASPNETConfiguration/ConnectionInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETConfiguration
+{
+    public class ConnectionInfo
+    {
+        public string Server { get; set; }
+
+        public string Database { get; set; }
+
+        public int Port { get; set; }
+    }
+}
---
+++ ASPNETConfiguration/src/ASPNETConfiguration/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace ASPNETConfiguration.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        IConfiguration _configuration;
+        ConnectionInfo _connectionInfo;
+        public ValuesController(IConfiguration configuration, IOptions<ConnectionInfo> connectionInfo)
+        {
+            _configuration = configuration;
+            _connectionInfo = connectionInfo.Value;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            // var serverName = _configuration.GetValue<string>("ConnectionInfo:Server");
+            var port = _connectionInfo.Port;
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
