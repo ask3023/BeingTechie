4b60ab4ce83dee2e8bb9486d99dbb2f3cea7b30b
ask3023 <sravan.ask@gmail.com>
2016-09-11 19:24:55 -0400

    Global Exception Logger

---
+++ ASPNETCoreGlobalException/src/ASPNETCoreLogging/Controllers/EmployeeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ASPNETCoreLogging.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        ILogger _logger;
+        public EmployeesController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<EmployeesController>();
+        }
+
+        // GET api/employees
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            throw new InvalidOperationException("Testing global exception logger...");
+            return new string[] { "Employee1", "Employee2" };
+        }
+
+        // GET api/employees/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/employees
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/employees/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/employees/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ASPNETCoreGlobalException/src/ASPNETCoreLogging/GlobalExceptionLogger.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreLogging
+{
+    public class GlobalExceptionLogger : IExceptionFilter
+    {
+        private ILogger _logger;
+
+        public GlobalExceptionLogger(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<GlobalExceptionLogger>();
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception.Message);
+        }
+    }
+}
