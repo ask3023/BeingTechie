f98f7f13bc881914b9f45fb862a36c930550ef40
ask3023 <sravan.ask@gmail.com>
2016-10-09 20:03:47 -0400

    Asp Net core environments sample

---
+++ ASPNETCoreEnvironments/src/ASPNETCoreEnvironments/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreEnvironments.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            throw new InvalidOperationException("Testing environment variable...");
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
