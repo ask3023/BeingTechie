9388d0724a97a30f64cc0ecbe9a417ba2314a118
ask3023 <sravan.ask@gmail.com>
2016-10-12 22:08:56 -0400

    ASP.NET Core OWIN Middleware component

---
+++ AspNetCoreCustomOwin/src/AspNetCoreCustomOwin/Infrastructure/CultureMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreCustomOwin.Infrastructure
+{
+    public class CultureMiddleware
+    {
+        private RequestDelegate _next;
+        private CultureMiddlewareOptions _options;
+        public CultureMiddleware(RequestDelegate next, CultureMiddlewareOptions options)
+        {
+            _next = next;
+            _options = options;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            string culture = context.Request.Query["culture"];
+
+            if (!string.IsNullOrWhiteSpace(culture))
+            {
+                CultureInfo cultureInfo = new CultureInfo(culture);
+
+                CultureInfo.CurrentCulture = cultureInfo;
+            }
+            else
+            {
+                CultureInfo.CurrentCulture = _options.DefaultCulture;
+            }
+
+            return _next(context);
+        }
+    }
+
+    public static class CultureMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCultureMiddlware(this IApplicationBuilder appBuilder)
+        {
+            return appBuilder.UseCultureMiddlware(new CultureMiddlewareOptions() { DefaultCulture = new CultureInfo("en-GB") });
+        }
+
+        public static IApplicationBuilder UseCultureMiddlware(this IApplicationBuilder appBuilder, CultureMiddlewareOptions options)
+        {
+            return appBuilder.UseMiddleware<CultureMiddleware>(options);
+        }
+    }
+}
---
+++ AspNetCoreCustomOwin/src/AspNetCoreCustomOwin/Infrastructure/CultureMiddlewareOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreCustomOwin.Infrastructure
+{
+    public class CultureMiddlewareOptions
+    {
+        public CultureInfo DefaultCulture { get; set; }
+    }
+}
